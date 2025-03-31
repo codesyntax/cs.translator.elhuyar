5e5793c648e6ccdaba6238f8804c58c44c9d4091
Ion Lizarazu <ilizarazu@codesyntax.com>
2025-03-31 13:52:14 +0200

    add @translate endpoint with the needed data to get the translation

---
+++ src/cs/translator/elhuyar/api/translator/post.py
@@ -0,0 +1,63 @@
+from cs.translator.elhuyar import _
+from plone.restapi.services import Service
+from plone import api
+import requests
+from plone.restapi.deserializer import json_body
+
+
+HEADERS = {"Accept": "application/json"}
+
+
+class Translator(Service):
+    def reply(self):
+        api_base_url = api.portal.get_registry_record(
+            "cs.translator.elhuyar.elhuyar_a_p_i_config.api_base_url"
+        )
+        api_id = api.portal.get_registry_record(
+            "cs.translator.elhuyar.elhuyar_a_p_i_config.api_id"
+        )
+        api_key = api.portal.get_registry_record(
+            "cs.translator.elhuyar.elhuyar_a_p_i_config.api_key"
+        )
+        translation_engine = api.portal.get_registry_record(
+            "cs.translator.elhuyar.elhuyar_a_p_i_config.translation_engine"
+        )
+        body = json_body(self.request)
+        language_pair = body.get("language_pair", None)
+        text = body.get("text", None)
+        if not language_pair or not text:
+            self.request.response.setStatus(400)
+            return {
+                "message": _(
+                    "Incorrect params, language_pair and text are required"
+                ),
+            }
+
+        result = requests.post(
+            f"{api_base_url}/translate_string",
+            json={
+                "api_id": api_id,
+                "api_key": api_key,
+                "translation_engine": translation_engine,
+                "language_pair": language_pair,  # Language of the original text and objective language for the translation: es-eu | eu-es | etc
+                "content_type": "html",  # hardcoded: Content type of the text: txt | html | xml
+                "text": text,
+            },
+            headers=HEADERS,
+        )
+        if result.ok:
+            # Ignoring keys:
+            # - "execution_time"
+            # - "glossary_active_words"
+            # - "interactive"
+            # - "original_text"
+            # - "source_sentences"
+            # - "translated_sentences"
+            # - "words"
+            return result.json().get("translated_text", "")
+        else:
+            try:
+                self.request.response.setStatus(result.status_code)
+                return result.json()
+            except Exception:
+                return {"message": _("Unexpected Error")}
