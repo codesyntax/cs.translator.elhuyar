0a8e65534af055079c88f7a6532fed78ea050b38
Ion Lizarazu <ilizarazu@codesyntax.com>
2025-03-31 15:53:04 +0200

    fix translations and add timeout handler

--- src/cs/translator/elhuyar/api/translator/post.py
+++ src/cs/translator/elhuyar/api/translator/post.py
@@ -1,4 +1,5 @@
 from cs.translator.elhuyar import _
+from zope.i18n import translate
 from plone.restapi.services import Service
 from plone import api
 import requests
@@ -6,6 +7,7 @@ from plone.restapi.deserializer import json_body
 
 
 HEADERS = {"Accept": "application/json"}
+TIMEOUT = 7
 
 
 class Translator(Service):
@@ -28,36 +30,46 @@ class Translator(Service):
         if not language_pair or not text:
             self.request.response.setStatus(400)
             return {
-                "message": _(
-                    "Incorrect params, language_pair and text are required"
+                "message": translate(
+                    _("Incorrect params, language_pair and text are required"),
+                    context=self.request,
                 ),
             }
+        try:
+            result = requests.post(
+                f"{api_base_url}/translate_string",
+                json={
+                    "api_id": api_id,
+                    "api_key": api_key,
+                    "translation_engine": translation_engine,
+                    "language_pair": language_pair,  # Language of the original text and objective language for the translation: es-eu | eu-es | etc
+                    "content_type": "html",  # hardcoded: Content type of the text: txt | html | xml
+                    "text": text,
+                },
+                headers=HEADERS,
+                timeout=TIMEOUT,
+            )
+            if result.ok:
+                # Ignoring keys:
+                # - "execution_time"
+                # - "glossary_active_words"
+                # - "interactive"
+                # - "original_text"
+                # - "source_sentences"
+                # - "translated_sentences"
+                # - "words"
+                return result.json().get("translated_text", "")
+            else:
+                try:
+                    self.request.response.setStatus(result.status_code)
+                    return result.json()
+                except Exception:
+                    return {
+                        "message": translate(
+                            _("Unexpected Error"), context=self.request
+                        )
+                    }
 
-        result = requests.post(
-            f"{api_base_url}/translate_string",
-            json={
-                "api_id": api_id,
-                "api_key": api_key,
-                "translation_engine": translation_engine,
-                "language_pair": language_pair,  # Language of the original text and objective language for the translation: es-eu | eu-es | etc
-                "content_type": "html",  # hardcoded: Content type of the text: txt | html | xml
-                "text": text,
-            },
-            headers=HEADERS,
-        )
-        if result.ok:
-            # Ignoring keys:
-            # - "execution_time"
-            # - "glossary_active_words"
-            # - "interactive"
-            # - "original_text"
-            # - "source_sentences"
-            # - "translated_sentences"
-            # - "words"
-            return result.json().get("translated_text", "")
-        else:
-            try:
-                self.request.response.setStatus(result.status_code)
-                return result.json()
-            except Exception:
-                return {"message": _("Unexpected Error")}
+        except requests.exceptions.Timeout:
+            self.request.response.setStatus(500)
+            return {"message": translate(_("Timeout"), context=self.request)}
