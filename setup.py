f39a78b90bef27064c0cb906bf4c2568ef9d3eb8
Mikel Larreategi <mlarreategi@codesyntax.com>
2026-01-16 14:31:55 +0100

    update to native namespaces

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 
 from setuptools import setup
 
+
 long_description = "\n\n".join(
     [
         open("README.md").read(),
--- src/cs/translator/elhuyar/upgrades/__init__.py
+++ src/cs/translator/elhuyar/upgrades/__init__.py
@@ -1,3 +1,4 @@
 import logging
 
+
 logger = logging.getLogger("cs.translator.elhuyar")
